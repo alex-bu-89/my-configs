47eabafabb7c010aceb465871acf7048a2b7dc88
Alex Buyanov <alex.bu.89@gmail.com>
2018-08-03 11:43:00 +0200

    change fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,14 @@
 # Nice light cyan color instead of dark blue
 set -gx LSCOLORS gxfxcxdxbxegedabagacad
 
+# Allow 256 colors in iTerm2 for pretty vim colors
+set -gx CLICOLOR 1
+set -gx TERM xterm-256color
+
+# Set GOPATH and GOROOT
+set -gx GOPATH $HOME/go
+set -gx GOROOT (go env GOROOT)
+
 # Override standard `ls`
 function ls --description 'List contents of directory'
   command ls -lFG $argv
@@ -11,11 +19,3 @@ function delmerged --description 'Delete all local branches that is already merg
   command git branch --merged | grep -v "\*" | grep -v "master" | xargs -n 1 git branch -d
   command git remote prune origin
 end
-
-# Set GOPATH and GOROOT
-set -gx GOPATH $HOME/go
-set -gx GOROOT (go env GOROOT)
-
-# Allow 256 colors in iTerm2 for pretty vim colors
-set -gx CLICOLOR 1
-set -gx TERM xterm-256color
